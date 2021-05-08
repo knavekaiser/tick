389bc441d885dd9ad34ee28171a69f542b825846
Naeem Ahmad <naeem.ahmad.9m@gmail.com>
2021-05-08 15:02:32 +0600

    console log omitted

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const fetch = require("node-fetch");
 
 const pingHeroku = setInterval(() => {
   fetch("https://fatwaarchive.herokuapp.com/")
-    .then((res) => {
-      console.log("pinged");
-    })
+    .then((res) => {})
     .catch((err) => {
       console.log("could not ping", err);
     });
